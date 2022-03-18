e39ff016d3dd8e678554237138120f5e4165dfb0
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-03-18 15:01:36 +0530

    DOM Manipulation

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,11 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading = document.querySelector('h1');
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.style.color = "black";
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll('hr');
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,27 +14,31 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs);
 // Set the border of the all the hr elements to "1px solid tomato"
-
+allHrsArray.setAttribute("class","hr");
 // Change the background of all the hr to "antiquewhite" using for of loop.
-
+for(elm of allHrsArray){
+  allHrsArray.setAttribute("background","hr");
+}
 // Change the 'border-radius' of all the hr to "5px" using array.
-
+for(elm of allHrsArray){
+  allHrsArray.setAttribute("border-radius","hr");
+}
 // Change the alignment of the heading(h1) to center.
-
+heading.style.textAlign = "center";
 // Change the font size of the heading to 3rem.
-
+heading.style.fontSize = "3rem";
 // Change the border of hr with class 'image' to `2px solid purple`.
-
+allHrsArray.setAttribute("image","2px solid purple");
 // Hide the box number 17 (last box).
 
 // Change the border of all the hr element from solid to dashed type
 
 // Create a pragraph element and store it in variable named 'para' using `createElement`
-
+let para = document.createElement("paragraph");
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
-
+para.innerText = "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure.";
 // Remove all the elements from box 1
 
 // Replace all the elements inside box 1 with the para (you created above)
